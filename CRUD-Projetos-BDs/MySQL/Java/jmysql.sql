af4ca42e3c1b19cbe030ba965355e586646c2ad2
luizdts <luizhadantas@gmail.com>
2022-04-06 00:44:33 -0300

    CRUDs in MySQL and PostgreSQL using Java.

---
+++ CRUD-Projetos-BDs/MySQL/Java/jmysql.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE jmysql;
+USE jmysql;
+
+CREATE TABLE produtos(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    preco DECIMAL(8,2) NOT NULL,
+    estoque INT NOT NULL
+);
